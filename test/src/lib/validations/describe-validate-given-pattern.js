34973f1fcc14a1a140a9ee331f24bb2950891347
Dansteve <danstevea@gmail.com>
2022-08-23 07:58:06 +0100

    make test simple

--- test/src/lib/validations/describe-validate-given-pattern.js
+++ test/src/lib/validations/describe-validate-given-pattern.js
@@ -10,47 +10,21 @@ describe('validations: validate given pattern', () => {
 	const sandbox = sinon.createSandbox();
 
 	const names = /^[a-zA-Z- ']+$/;
-	const zipCodes = /^[0-9']{5}$/;
-	const dates = /^[0-9]{4}-[0-9]{2}-[0-9]{2}$/;
-	const ifsCodes = /^[A-Za-z]{4}\d{7}$/;
+	const positive = { type: 'names', input: 'Dansteve Adekanbi', regex: names };
+	const negative = { type: 'names', input: 'Species 8472', regex: names };
 
-	const positive = [
-		{ type: 'names', input: 'Dansteve la forge', regex: names },
-		{ type: 'names', input: 'Jean-Luc Picard', regex: names },
-		{ type: 'names', input: 'WILLIAM RIKER', regex: names },
-		{ type: 'zipcodes', input: '12345', regex: zipCodes },
-		{ type: 'dates', input: '2019-01-01', regex: dates },
-		{ type: 'ifsCodes', input: 'ABCD1234567', regex: ifsCodes }
-	];
+	it(`should return true for well-formatted ${positive.type} (${positive.input})`, () => {
+		const expected = true;
+		const actual = validateGivenPattern(positive.regex)(positive.input);
 
-	const negative = [
-		{ type: 'names', input: 'Species 8472', regex: names },
-		{ type: 'names', input: 'picard@enterprise.com', regex: names },
-		{ type: 'names', input: 'WILLIAM RIKER !!!!', regex: names },
-		{ type: 'zipcodes', input: '12', regex: zipCodes },
-		{ type: 'zipcodes', input: '1234567', regex: zipCodes },
-		{ type: 'dates', input: '2019-01-DD', regex: dates },
-		{ type: 'ifsCodes', input: '124ABCD12345678', regex: ifsCodes }
-	];
-
-	positive.forEach((item) => {
-		const { type, input, regex } = item;
-		it(`should return true for well-formatted ${type} (${input})`, () => {
-			const expected = true;
-			const actual = validateGivenPattern(regex)(input);
-
-			expect(actual).to.be.equal(expected);
-		});
+		expect(actual).to.be.equal(expected);
 	});
 
-	negative.forEach((item) => {
-		const { type, input, regex } = item;
-		it(`should return false for badly formatted ${type} (${input})`, () => {
-			const expected = false;
-			const actual = validateGivenPattern(regex)(input);
+	it(`should return false for badly formatted ${negative.type} (${negative.input})`, () => {
+		const expected = false;
+		const actual = validateGivenPattern(negative.regex)(negative.input);
 
-			expect(actual).to.be.equal(expected);
-		});
+		expect(actual).to.be.equal(expected);
 	});
 
 	afterEach(() => {
