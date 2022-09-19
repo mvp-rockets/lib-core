19567d1312c0cf8ad9d3df0dc808804a7760c13b
Sagar Bm <sagar.mangasuli@napses.com>
2022-09-19 21:39:05 +0530

    Revert "Replaced `composeP` to `composeWith`"
    This reverts commit 477e3286b32f44a6fd558d1f4d2f77f951d4ffa7.

--- src/lib/utilities/compose-result.js
+++ src/lib/utilities/compose-result.js
@@ -1,13 +1,13 @@
-const R = require("ramda");
-const Result = require("folktale/result");
-const whenResult = require("./whenResult");
-const { ifElse } = require("./ifElse");
+const R = require('ramda');
+const Result = require('folktale/result');
+const whenResult = require('./whenResult');
+const { ifElse } = require('./ifElse');
 
 const executeIfResultIsSuccessful = R.curry(async (fn, result) => {
 	if (Result.hasInstance(result)) {
 		return whenResult((value) => fn(value))(result);
 	}
-	return Result.Error("Return type of execution was not type of folktale/result.");
+	return Result.Error('Return type of execution was not type of folktale/result.');
 });
 
 /*
@@ -18,14 +18,17 @@ FIXME: Instead of composeP we need to use composeWith Reference: https://github.
  */
 const composeResult = function () {
 	if (arguments.length === 0) {
-		throw new Error("onSuccess requires at least one argument");
+		throw new Error('onSuccess requires at least one argument');
 	}
 	const init = Array.prototype.slice.call(arguments);
 	const last = init.pop();
 	return ifElse(
 		R.isEmpty,
-		() => R.composeWith(R.andThen)([last]),
-		() => R.composeWith(R.andThen)([R.composeWith(R.andThen)([]).apply(this, R.map(executeIfResultIsSuccessful, init)), last])
+		() => R.composeP(last),
+		() => R.composeP(
+			R.composeP.apply(this, R.map(executeIfResultIsSuccessful, init)),
+			last
+		)
 	)(init);
 };
 module.exports = composeResult;
